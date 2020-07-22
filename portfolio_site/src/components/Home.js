6d64815001cebc659298d52d49d99e4417dc31c1
harryman20 <harrisonsmith646@gmail.com>
2020-07-22 10:35:33 -0400

    New Commits

---
+++ portfolio_site/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = function() {
+  return (
+    <div>
+      <h1 className="container-headers">- Our Mission and Vision -</h1>
+      <p>
+        Blah, blah.. this going to work?
+      </p>
+    </div>
+  )
+}
+
+export default Home;
