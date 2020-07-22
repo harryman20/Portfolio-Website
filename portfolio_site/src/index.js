ee1701b187a8406cee1647d6ba4ad2dd55d021a3
harryman20 <harrisonsmith646@gmail.com>
2020-07-22 12:00:57 -0400

    Another Commit

--- portfolio_site/src/index.js
+++ portfolio_site/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import Main from "./Main";
+import S from "./Main";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Main />
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
