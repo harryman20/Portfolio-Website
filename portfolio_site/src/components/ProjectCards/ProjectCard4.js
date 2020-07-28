f243fc6b6b481a4c38dd310f55bafc19c715e642
harryman20 <harrisonsmith646@gmail.com>
2020-07-28 14:12:40 -0400

    Fixing Up contentcards

---
+++ portfolio_site/src/components/ProjectCards/ProjectCard4.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../Projects.css';
+
+export default function ProjectCard4(props) {
+  return (
+    <div id="card-wrapper" className="card-wrapper">
+      <div id="card-content">
+        <h2>{props.name}</h2>
+        <h3>{props.body}</h3>
+        <h4>{props.image}</h4>
+        <button>{props.footer}</button>
+      </div>
+    </div>
+  );
+}
--- portfolio_site/src/components/Projects.js
+++ portfolio_site/src/components/Projects.js
@@ -1,19 +1,45 @@
 import React from 'react';
-import data from './ProjectData';
-import ProjectCard from './ProjectCard';
+import ProjectCard1 from './ProjectCards/ProjectCard1';
+import ProjectCard2 from './ProjectCards/ProjectCard2';
+import ProjectCard3 from './ProjectCards/ProjectCard3';
+import ProjectCard4 from './ProjectCards/ProjectCard4';
 
 function Project() {
   return (
     <div className="wrapper">
-      <h1>Projects</h1>
-      {data.map(project => (
-        <ProjectCard
-          key={project.name}
-          name={project.name}
-          body={project.body}
-          image={project.image}
-          footer={project.footer}/>
-      ))}
+      <div id="rightTitle">
+        <h1>My Projects</h1>
+      </div>
+      <div id="leftCards">
+          <ProjectCard1
+            key= '1'
+            id='1'
+            name='Project1'
+            body='This project was a practise project where I designed a website for a vacuum company.'
+            image='image'
+            footer='footer' />
+          <ProjectCard2
+            key= '2'
+            id='2'
+            name='Project2'
+            body='This project was a practise project where I designed a website for a vacuum company.'
+            image='image'
+            footer='footer' />
+          <ProjectCard3
+            key= '3'
+            id='3'
+            name='Project3'
+            body='This project was a practise project where I designed a website for a vacuum company.'
+            image='image'
+            footer='footer' />
+          <ProjectCard4
+            key= '4'
+            id='4'
+            name='Project4'
+            body='This project was a practise project where I designed a website for a vacuum company.'
+            image='image'
+            footer='footer' />
+      </div>
     </div>
   )
 }
