9565fa1751adfd08c6a071a94a85b38557e3f301
harryman20 <harrisonsmith646@gmail.com>
2020-08-06 16:48:19 -0400

    Email setup

--- portfolio_site/src/components/Contact.js
+++ portfolio_site/src/components/Contact.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
+import emailjs from 'emailjs-com';
 
 class Contact extends Component {
   render() {
+      function sendEmail(e) {
+      e.preventDefault();
+
+      emailjs.sendForm('default_service', 'real', e.target, 'user_S1owFNY9l8W7ZE3OKqia1')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
     return (
-      <div>
-        <h2>GOT QUESTIONS?</h2>
-        <h3>How to contact me on Social media</h3>
-        <p>twitter</p>
-        <p>github</p>
-        <p>facebook</p>
-        <p>linkedin</p>
-        <form>
-          <h1>Email Inquiry Form</h1>
-          <label for="name">
-            Name:
-            <input name="name" type="text" />
-          </label>
-          <label for="email">
-            Email:
-            <input name="email"type="email" />
-          </label>
-          <input type="submit"></input>
-          <input type="textarea"></input>
-        </form>
-      </div>
+      <form className="contact-form" onSubmit={sendEmail}>
+        <input type="hidden" name="contact_number" />
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
     );
   }
 }
