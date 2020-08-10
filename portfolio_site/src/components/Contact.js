124effea94f4c77080e37f317bf4c286bc056fd0
harryman20 <harrisonsmith646@gmail.com>
2020-08-10 15:48:22 -0400

    Updating and working on contact page

--- portfolio_site/src/components/Contact.js
+++ portfolio_site/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import emailjs from 'emailjs-com';
+import './Contact.css';
 
 class Contact extends Component {
   render() {
@@ -15,14 +16,26 @@ class Contact extends Component {
     }
     return (
       <form className="contact-form" onSubmit={sendEmail}>
-        <input type="hidden" name="contact_number" />
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+        <div className="formContainer">
+          <input type="hidden" name="contact_number" />
+          <div className="inputCon">
+            <label htmlFor="user_name">Name*: </label>
+            <input type="text" name="user_name" required />
+          </div>
+          <div className="inputCon">
+            <label htmlFor="user_email">Email*: </label>
+            <input type="email" name="user_email" required />
+          </div>
+          <div className="inputCon">
+            <label htmlFor="phone">Phone-Number: </label>
+            <input type="phone" name="phone" required />
+          </div>
+          <div className="inputCon">
+            <label htmlFor="message">Message*: </label>
+            <textarea name="message"/>
+          </div>
+          <input type="submit" value="Send" />
+        </div>
       </form>
     );
   }
