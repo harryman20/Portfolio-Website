00226147df9027839f444e3cd2e0d94e9d27950c
harryman20 <harrisonsmith646@gmail.com>
2020-08-18 17:29:16 -0400

    Working on Contact Pages

--- portfolio_site/src/components/Contact.js
+++ portfolio_site/src/components/Contact.js
@@ -1,12 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import emailjs from 'emailjs-com';
 import './Contact.css';
 import "./Home.css";
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from '@hookform/error-message';
 
 export default function Contact() {
-    const { register, handleSubmit, watch, errors } = useForm();
     function sendEmail(e) {
       e.preventDefault();
 
@@ -20,69 +17,64 @@ export default function Contact() {
 
     return (
       <div>
-        <form className="contact-form" onSubmit={handleSubmit(sendEmail)}>
-          <div className="column left">
-            <p></p>
-          </div>
-          <div className="column middle">
-            <div className="titleContainer">
-              <h1>
-                Contact Me!
-              </h1>
-              <p id="pText">
-                Fill out the form below to send me an email. I will respond as soon as I can. Please provide your email and/or your phone number so that I will be able to get back to you. Thank you for viewing my portfolio!
-              </p>
-            </div>
-            <div className="column middle-left">
-              <input type="hidden" name="contact_number" />
-              <div className="inputCon">
-                <label htmlFor="user_name">Name*: </label>
-                <input type="text" ref={register({ required: "This is required" })} name="user_name" />
-                <br />
-                <ErrorMessage errors={errors} name="user_name">
-                  {({ message }) => <p id="usernameError">{message}</p>}
-                </ErrorMessage>
-              </div>
-              <div className="inputCon">
-                <label htmlFor="user_email">Email*: </label>
-                <input refs="email" ref={register({ required: "This is required" })} type="email" name="user_email" />
-                <br />
-                <ErrorMessage errors={errors} name="user_email">
-                  {({ message }) => <p id="usernameError">{message}</p>}
-                </ErrorMessage>
-              </div>
-              <div className="inputCon">
-                <label htmlFor="phone">Phone-Number: </label>
-                <input type="phone" name="phone" />
-              </div>
-              <div className="inputCon">
-                <label htmlFor="message">Message*: </label>
-                <textarea ref={register({ required: "This is required" })} name="message"/>
-                <br />
-                <ErrorMessage errors={errors} name="user_email">
-                  {({ message }) => <p id="usernameError">{message}</p>}
-                </ErrorMessage>
-              </div>
-              <input className="submitButton" type="submit" value="Send" />
-            </div>
-            <div className="column middle-right">
-              <div className="rightContent">
-                <h2>My Contact Information</h2>
-                <h3>Social Media Links</h3>
-                <h4>Twitter</h4>
-                <h4>Facebook</h4>
-                <h4>LinkedIn</h4>
-                <h4>GitHub</h4>
-                <h3>Additional Information</h3>
-                <h4>Telephone: 1 519 890 2057</h4>
-                <h4>Email: harrisonsmith646@gmail.com</h4>
-                <h4>You can also reach out to me on discord. (request discord name in message on the left)</h4>
-              </div>
-            </div>
-          </div>
-          <div className="column right">
-          </div>
-        </form>
-      </div>
+       <form className="contact-form" onSubmit={sendEmail}>
+         <div className="column left">
+           <p></p>
+         </div>
+         <div className="column middle">
+           <div className="titleContainer">
+             <h1>
+               Contact Me!
+             </h1>
+             <p id="pText">
+               Fill out the form below to send me an email. I will respond as soon as I can. Please provide your email and/or your phone number so that I will be able to get back to you. Thank you for viewing my portfolio!
+             </p>
+           </div>
+           <div className="column middle-left">
+             <input type="hidden" name="contact_number" />
+             <div className="inputCon">
+               <label htmlFor="user_name">Name*: </label>
+               <input type="text" name="user_name" required />
+             </div>
+             <div className="inputCon">
+               <label htmlFor="user_email">Email*: </label>
+               <input type="email" name="user_email" required />
+             </div>
+             <div className="inputCon">
+               <label htmlFor="phone">Phone-Number: </label>
+               <input type="phone" name="phone" required />
+             </div>
+             <div className="inputCon">
+               <label htmlFor="message">Message*: </label>
+               <textarea name="message"/>
+             </div>
+             <input className="submitButton" type="submit" value="Send" />
+           </div>
+           <div className="column middle-right">
+             <div className="rightContent">
+                <div className="rightConHeader">
+                  <h1>My Contact Information</h1>
+                  <hr></hr>
+                </div>
+                <div className="rightConContent">
+                  <h2>Social Media Links</h2>
+                    <a className="socialImages" href=""><img className="imagesForSocial" src="./icons8-facebook-48.png" alt="facebook"/></a>
+                    <a className="socialImages" href="https://www.linkedin.com/in/harry-smith-a97a29188/"><img className="imagesForSocial" src="./icons8-linkedin-48.png" alt="linkedin" /></a>
+                    <a className="socialImages" href="https://twitter.com/harrymans97"><img className="imagesForSocial" src="./icons8-twitter-squared-48.png" alt="twitter" /></a>
+                    <a className="socialImages" href="https://github.com/harryman20"><img className="imagesForSocial" src="./icons8-github-48.png" alt="gitHub" /></a>
+                  <h2>Additional Information</h2>
+                  <hr></hr>
+                  <h4>Telephone: 1 519 890 2057</h4>
+                  <h4>Email: harrisonsmith646@gmail.com</h4>
+                  <h4>You can also reach out to me on discord. (request discord name in message on the left)</h4>
+                </div>
+             </div>
+           </div>
+         </div>
+         <div className="column right">
+           <p></p>
+         </div>
+       </form>
+     </div>
     );
   }
